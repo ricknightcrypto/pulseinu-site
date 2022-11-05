64671e5be8513001bb4dda96c7a538267088cead
rick <moneroj67@protonmail.com>
2022-11-05 21:00:58 +0400

    wip

--- assets/javascripts/utility_functions.js
+++ assets/javascripts/utility_functions.js
@@ -1,4 +1,3 @@
-
 // TODO: Constant here to be used everywhere
 const tokenAddress = "0xCaB910925CD92DA4aD5F7D3F1b97078fC43328CB";
 const tokenSymbol = "PINU";
@@ -6,12 +5,11 @@ const tokenDecimals = 12;
 const tokenImage = "https://pulseinu.org/assets/images/pinu_small.png";
 
 async function addTokenFunction() {
-
   try {
     const wasAdded = await ethereum.request({
-      method: 'wallet_watchAsset',
+      method: "wallet_watchAsset",
       params: {
-        type: 'ERC20',
+        type: "ERC20",
         options: {
           address: tokenAddress,
           symbol: tokenSymbol,
@@ -22,11 +20,11 @@ async function addTokenFunction() {
     });
 
     if (wasAdded) {
-      console.log('PINU Added to Metamask');
+      console.log("PINU Added to Metamask");
     } else {
-      console.log('PINU Not Added to Metamask');
+      console.log("PINU Not Added to Metamask");
     }
   } catch (error) {
     console.log(error);
   }
-  }
+}
